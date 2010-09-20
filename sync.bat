ae15144d12a17fa5821d34d8e2b5f7bc0da1f373
ktmud <jyyjcc@gmail.com>
2010-09-20 10:01:16 +0800

    Automated commit on 2010/09/20 周一  10:01

--- sync.bat
+++ sync.bat
@@ -1,5 +1,5 @@
 @echo off
-color 30
+color 2F
 title 通过 Git 同步.. 
 ::Start...
 echo Start synchronizing...
@@ -17,6 +17,6 @@ call git commit -a -m "Automated commit on %fvar%"
 
 :: check if ssh-agent is running, if not, open git bash and login.
 tasklist|findstr /i "ssh-agent.exe" || title "Passphrase needed." 
-    \ && echo "Please enter ssh passphrase and push manunally" 
-    \ && cmd /c ""C:\Program Files\Git\bin\sh.exe" --login -i"
+    && echo "Please enter ssh passphrase and push manunally" 
+    && cmd /c ""C:\Program Files\Git\bin\sh.exe" --login -i"
 call git push origin master
