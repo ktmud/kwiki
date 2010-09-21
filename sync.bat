396d3e84725691cf28fe9390546493ed7b884faf
ktmud <jyyjcc@gmail.com>
2010-09-21 14:11:31 +0800

    - 2010/09/21 周二

--- sync.bat
+++ sync.bat
@@ -1,17 +1,19 @@
 @echo off
-title 閫氳繃 Git 鍚屾.. 
-::Start...
+title 通过 Git 同步.. 
 echo Start synchronizing...
-
-echo Commit changes...
 
 :: get date and time 
 for /f "delims=" %%a in ('date/t') do @set mydate=%%a 
 for /f "delims=" %%a in ('time/t') do @set mytime=%%a 
-set fvar=%mydate%%mytime% 
+set msg=%mydate%%mytime% 
 
 :: add all new files 
 call git add . 
-call git commit -a -m "%fvar%"
+
+:: 请求输入 commit 消息
+
+Set /p cmsg=请输入Commit注释（留空则为提交时间）: 
+if not {%cmsg%}=={''} set msg=%cmsg%
+call git commit -a -m "%msg% - %mydate%"
 
 call git push origin master
