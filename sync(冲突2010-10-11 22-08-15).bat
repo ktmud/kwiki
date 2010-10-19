553293955a73071738138d1c7f5e1f70f7f6a94e
ktmud <jyyjcc@gmail.com>
2010-10-19 14:05:59 +0800

    I'm on ubuntu now

---
+++ sync(冲突2010-10-11 22-08-15).bat
@@ -0,0 +1,25 @@
+@echo off
+title 同步到github.. 
+
+cls
+echo 开始同步...
+
+:: get date and time 
+for /f "delims=" %%a in ('date/t') do @set mydate=%%a 
+for /f "delims=" %%a in ('time/t') do @set mytime=%%a 
+set date=%mydate%%mytime% 
+
+:: add all new files 
+call git add . 
+
+:: 请求输入 commit 消息
+Set /p cmsg=请输入Commit注释（留空则为提交时间）: 
+:: bat语法实在是太戳了
+call git commit -a -m "%cmsg%_%date%"
+REM echo 开始推送到github/ktmud/kwiki/...
+REM call git push origin
+REM echo 开始推送到kwiki
+REM call git push kwiki
+echo 开始推送到Web
+call git push web 
+exit
