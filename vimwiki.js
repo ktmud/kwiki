07d4b69b95c68069549768618456e4e9eecfb025
ktmud <jyyjcc@gmail.com>
2010-12-01 12:07:11 +0800

    add syntax highlighter

--- vimwiki.js
+++ vimwiki.js
@@ -1,67 +1,66 @@
-var HOST_ROOT = 'http://wiki.ktmud.com/',
-kwiki_identifier = 'wiki' + window.location.pathname.replace(/\//g, '_').replace('index.html', '').replace('.html', '');
-
-var kwiki = kwiki || {};
-
-kwiki.getCSS = function(url) {
-    var tag = '<link rel="stylesheet" href="' + url + '" />';
-    $('body').append(tag);
-};
-
-kwiki.insertCSS = function(url) {
-    $.get(url, function(res) {
-        var tag = '<style>' + res + '</stle>';
+var kwiki = {
+    getCSS: function(url) {
+        var tag = '<link rel="stylesheet" href="' + url + '" />';
         $('body').append(tag);
-    });
-};
+    },
+    insertCSS: function(url) {
+        $.get(url, function(res) {
+            var tag = '<style>' + res + '</stle>';
+            $('body').append(tag);
+        });
+    },
+    insertScript: function(url, bottle) {
+        var tag = '<script src=' + url + '></script>';
+        if (!bottle) bottle = 'head';
+        $(bottle).append(tag);
+    },
+    loadCmt: function(provider) {
+        if ($('#no-comment').length > 0) return;
 
-kwiki.insertScript = function(url, bottle) {
-    var tag = '<script src=' + url + '></script>';
-    if (!bottle) bottle = 'head';
-    $(bottle).append(tag);
-};
+        var thread, extraCSS, theJS;
 
-kwiki.loadCmt = function(provider) {
-    if ($('#no-comment').length > 0) return;
-
-    var thread, extraCSS, theJS;
-
-    if (provider == 'disqus') {
-        window.disqus_iframe_css = HOST_ROOT + 'disqus_iframe.css';
-        window.disqus_identifier = window.kwiki_identifier || undefined;
-        thread = $('<div id="disqus_thread">');
-        extraCSS = HOST_ROOT + 'disqus.css';
-        //theJS = HOST_ROOT + 'disqus_loader.js';
-        theJS = 'http://kwiki.disqus.com/embed.js';
-    } else if (provider == 'intensedebate') {
-        window.idcomments_acct = '5a60a7c6da8072e3edcb67fb0e7850e9';
-        window.idcomments_post_id = kwiki_identifier;
-        window.idcomments_post_url = undefined;
-        thread = $('<div id="idc-container"><span class="startloading">Loading comments..<span></div>');
-        //extraCSS = HOST_ROOT + 'intensedebate.css';
-        theJS = 'http://www.intensedebate.com/js/genericCommentWrapperV2.js';
-    } else {
-        return;
-    }
-
-    $('#main').append(thread);
-
-    var win = $(window);
-
-    function load() {
-        if (win.scrollTop() + win.height() > thread.offset().top - 250) {
-
-            if (extraCSS) kwiki.getCSS(extraCSS);
-
-            $.getScript(theJS);
-
-            win.unbind('scroll');
+        if (provider == 'disqus') {
+            window.disqus_iframe_css = HOST_ROOT + 'disqus_iframe.css';
+            window.disqus_identifier = window.kwiki_identifier || undefined;
+            thread = $('<div id="disqus_thread">');
+            extraCSS = HOST_ROOT + 'disqus.css';
+            //theJS = HOST_ROOT + 'disqus_loader.js';
+            theJS = 'http://kwiki.disqus.com/embed.js';
+        } else if (provider == 'intensedebate') {
+            window.idcomments_acct = '5a60a7c6da8072e3edcb67fb0e7850e9';
+            window.idcomments_post_id = kwiki_identifier;
+            window.idcomments_post_url = undefined;
+            thread = $('<div id="idc-container"><span class="startloading">Loading comments..<span></div>');
+            //extraCSS = HOST_ROOT + 'intensedebate.css';
+            theJS = 'http://www.intensedebate.com/js/genericCommentWrapperV2.js';
+        } else {
+            return;
         }
-    }
 
-    win.bind('scroll', load);
-    load();
+        $('#main').append(thread);
+
+        var win = $(window);
+
+        function load() {
+            if (win.scrollTop() + win.height() > thread.offset().top - 250) {
+
+                if (extraCSS) kwiki.getCSS(extraCSS);
+
+                $.getScript(theJS);
+
+                win.unbind('scroll');
+            }
+        }
+
+        win.bind('scroll', load);
+        load();
+    }
 };
+
+var HOST_ROOT = 'http://wiki.ktmud.com/',
+kwiki_identifier = 'wiki' + window.location.pathname.replace(/\//g, '_')
+    .replace('index.html', '')
+    .replace('.html', '');
 
 $(document).ready(function() {
     if (window.innerWidth >= 460) {
@@ -140,6 +139,31 @@ $(document).ready(function() {
     //评论
     if (!$.browser.msie) kwiki.loadCmt('disqus');
 
+    //代码高亮
+    var syntaxHighlight = function() {
+        kwiki.getCSS('http://alexgorbatchev.com/pub/sh/current/styles/shThemeRDark.css');
+        $.getScript('http://alexgorbatchev.com/pub/sh/current/scripts/shCore.js', function() {
+            $.getScript('http://alexgorbatchev.com/pub/sh/current/scripts/shAutoloader.js', function(){
+                SyntaxHighlighter.config.strings.expandSource = '+ 展开源码';
+                SyntaxHighlighter.config.strings.viewSource = '新窗口查看源码';
+                SyntaxHighlighter.config.strings.copyToClipboard = '复制到剪贴板';
+                SyntaxHighlighter.config.strings.copyToClipboardConfirmation = '已经复制源码到剪切板';
+                SyntaxHighlighter.config.strings.print = '打印';
+                SyntaxHighlighter.config.strings.help = '';
+                SyntaxHighlighter.config.strings.alert = '语法高亮器&lt;br /&gt;';
+                SyntaxHighlighter.config.strings.noBrush = '找不到下列语言的渲染器: ';
+                SyntaxHighlighter.autoloader(
+                  'js jscript javascript  http://alexgorbatchev.com/pub/sh/current/scripts/shBrushJScript.js',
+                  'php http://alexgorbatchev.com/pub/sh/current/scripts/shBrushPhp.js',
+                  'css CSS Css http://alexgorbatchev.com/pub/sh/current/scripts/shBrushCss.js',
+                  'Bash bash http://alexgorbatchev.com/pub/sh/current/scripts/shBrushBash.js'
+                );
+                 
+                SyntaxHighlighter.all();
+            });
+        });
+    };
+    syntaxHighlight();
 });
 
 //Google 统计
